2ecf74d7a3b4abb2a8f71253b794b3c6cfbfb779
Bodri Mark <markbodri@gmail.com>
2022-09-30 15:23:24 +0200

    Initial commit

---
+++ scripts/listener.py
@@ -0,0 +1,14 @@
+import rospy
+from std_msgs.msg import String
+def callback(data):
+	print(rospy.get_caller_id() + "I heard ", data.data)
+	
+def listener():
+	rospy.init_node('listener', anonymous=True)
+	
+	rospy.Subscriber("chatter", String, callback)
+	
+	rospy.spin()
+	
+if __name__ == '__main__':
+	listener()
